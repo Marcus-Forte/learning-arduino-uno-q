919655d20e7f8f47aee8d32d75ec9c91ae01287c
Marcus Forte <davi2812@hotmail.com>
2026-08-19 01:48:11 +0200

    runtime dockerfile gcc-16 lib add. added arg option to grpc server bridge simulator

--- src/bridge/main.cc
+++ src/bridge/main.cc
@@ -1,7 +1,9 @@
 #include <grpcpp/grpcpp.h>
+#include <unistd.h>
 
 #include <array>
 #include <iostream>
+#include <memory>
 #include <string>
 
 #include "bridge.hh"
@@ -63,15 +65,31 @@ private:
   Bridge bridge_;
 };
 
-int main() {
+int main(int argc, char *argv[]) {
+  bool simulation = false;
+  int opt;
+  while ((opt = getopt(argc, argv, "sh")) != -1) {
+    if (opt == 's') {
+      simulation = true;
+    } else if (opt == 'h' || opt == '?') {
+      std::cout << "Usage: " << argv[0] << " [-s] [-h]\n"
+                << "  -s  Run with the simulated MCU service\n"
+                << "  -h  Show this help message\n";
+      return opt == 'h' ? 0 : 1;
+    }
+  }
 
   const std::string server_address("0.0.0.0:50051");
-  // McuServiceSimulator service;
-  McuServiceImpl service;
+  std::shared_ptr<McuService::Service> service;
+  if (simulation) {
+    service = std::make_shared<McuServiceSimulator>();
+  } else {
+    service = std::make_shared<McuServiceImpl>();
+  }
 
   grpc::ServerBuilder builder;
   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  builder.RegisterService(&service);
+  builder.RegisterService(service.get());
 
   std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
   if (!server) {
